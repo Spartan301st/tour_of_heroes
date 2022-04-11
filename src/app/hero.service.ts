3fe3ea49b6bc2f4fbc35557e6cb5df2e1ec1a833
Spartan301st <anilgajiev@gmail.com>
2022-04-11 11:00:08 +0400

    Commit 3 - create 2 services (Hero/Message) and inject them; handling asyncronicity

---
+++ src/app/hero.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+import { MessageService } from './message.service';
+
+// for simulating asyncronousness
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  constructor(private messageService: MessageService) {}
+
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+  // for
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
 
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,13 +16,32 @@ export class HeroesComponent implements OnInit {
   //   name: 'Leonidas',
   // };
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+
+  // heroes = HEROES;
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
-  heroes = HEROES;
+  // when user is selected add a corresponding message info
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  // to be able to handle Observable returned by our service
+  getHeroes(): void {
+    // here we are subscribing for the observable returned
+    // The subscribe() method passes the emitted array to the callback, which sets the component's heroes property.
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
 }
