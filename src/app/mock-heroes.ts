dcab4ac8b78155f82c47f3ccd8eb462906108155
Spartan301st <anilgajiev@gmail.com>
2022-04-11 09:30:00 +0400

    Commit 1 - display heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Gandalf' },
+  { id: 12, name: 'Frodo' },
+  { id: 13, name: 'Elrond' },
+  { id: 14, name: 'Aragorn' },
+  { id: 15, name: 'Legolas' },
+  { id: 16, name: 'Sam' },
+  { id: 17, name: 'Eowyn' },
+  { id: 18, name: 'Gimli' },
+  { id: 19, name: 'Merry' },
+  { id: 20, name: 'Galadriel' },
+];
